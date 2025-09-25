d67bababe9bc98b0886ec59e344708a5ad533e89
yejinleee <yejin9487@gmail.com>
2025-09-26 07:48:29 +0900

    feat: update document head with new title, description, and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-  <link
-    rel="stylesheet"
-    as="style"
-    crossOrigin="anonymous"
-    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css"
-  />
-</Head>
+        <title>장사살랑 - 제주도 소상 공인 멘토링 플랫폼</title>
+        <meta name="description" content="제주도 소상 공인 멘토링 플랫폼" />
+        <link rel="icon" href="/mainIcons.svg" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
